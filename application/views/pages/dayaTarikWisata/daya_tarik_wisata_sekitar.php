e5ca83534293305cd94f359bda873e57a61736ab
gensza <mohamadgenszavernando@gmail.com>
2023-04-09 14:03:23 +0700

    update last kaynaya

--- application/views/pages/dayaTarikWisata/daya_tarik_wisata_sekitar.php
+++ application/views/pages/dayaTarikWisata/daya_tarik_wisata_sekitar.php
@@ -1,4 +1,4 @@
-<div class="hero-wrap js-fullheight" style="background-image: url('assets/images/dayaTarikWisata/daya-tarik-wisata-10.jpg');">
+<div class="hero-wrap js-fullheight" style="background-image: url(<?= base_url('assets/images/dayaTarikWisata/daya-tarik-wisata-10.jpg') ?>);">
     <div class="overlay"></div>
     <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
